a9dd4e4a34dd7a7b27997b0ae11875884bd03d1d
Bharath <nallagondlabharath159@gmail.com>
2025-11-12 22:11:43 +0530

    Update join room ws server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,24 +1,65 @@
-import { WebSocketServer } from "ws";
-
-const wss = new WebSocketServer({ port: 8080 });
+import { WebSocketServer, WebSocket } from "ws";
 import { JWT_SCRET } from "@repo/backend-common/config";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { prismaClient } from "@repo/db/client";
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws, request) => {
-  const url = request.url;
-  if (!url) return;
+interface Users {
+  userId: string;
+  rooms: string[];
+  ws: WebSocket;
+}
+const users: Users[] = [];
 
+const getUserId = (url: string) => {
   const queryParams = new URLSearchParams(url.split("?")[1]);
   const token = queryParams.get("token") ?? "";
 
   const decoded = jwt.verify(token, JWT_SCRET);
 
   if (!decoded || typeof decoded === "string" || !decoded.userId) {
+    return null;
+  }
+
+  return decoded.userId;
+};
+
+wss.on("connection", (ws, request) => {
+  const url = request.url;
+  if (!url) return;
+
+  const userId = getUserId(url);
+
+  if (!userId) {
     ws.close();
     return;
   }
 
-  ws.on("message", (data) => {
-    ws.send("pong");
+  users.push({
+    userId,
+    ws,
+    rooms: [],
+  });
+
+  ws.on("message", async (data) => {
+    const parsedData = JSON.parse(data as unknown as string);
+
+    if (parsedData.type == "join_room") {
+      const roomExist = await prismaClient.room.findUnique({
+        where: {
+          id: parsedData.roomId,
+        },
+      });
+      if (!roomExist) {
+        ws.send(JSON.stringify({ type: "error", message: "Room Not Found" }));
+        return;
+      }
+      const user = users.find((x) => x.ws == ws);
+      if (!user) {
+        ws.send(JSON.stringify({ type: "error", message: "User Not Found" }));
+        return;
+      }
+      user.rooms = [...user?.rooms, parsedData.roomId];
+    }
   });
 });
